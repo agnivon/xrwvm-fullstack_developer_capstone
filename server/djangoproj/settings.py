8607dfed5b7fb55b69f4a5acec568799cba62423
agnivon <agnivon@gmail.com>
2025-05-08 11:17:39 -0400

    deployment config

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,10 +31,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://agnivon-8000.theiadockernext-1-la\
-    bs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://agnivon-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://agnivon-8000.theiadockernext-1-la\bs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://agnivon-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
